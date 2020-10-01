4156e58411dbe75764deeec0ea494ff7d490222f
HIRENLALAKIYA <hiren.lalakiya009@gmail.com>
2020-10-01 11:25:02 +0530

    First commit

--- App.js
+++ App.js
@@ -1,21 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
+import FeedNavigator from "./navigation/FeedNavigator";
+import feedReducer from "./store/reducers/Feed";
+
+const rootReducer = combineReducers({
+  feed: feedReducer,
+});
+
+const store = createStore(rootReducer);
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <FeedNavigator />
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
